7d33eeefcba6d9c20b487f5deb100cbc66b32ee1
Fabricio Oliveira <fabricio_fer13@outlook.com>
2018-04-24 17:03:12 -0300

    Arquivo welcome modificado

--- welcome.sh
+++ welcome.sh
@@ -2,5 +2,16 @@
 clear;
 echo -n "Digite seu nome:" 
 read nome;
-echo "Hello "$nome;
-
+echo -n "Digite sua idade:";
+read idade;
+clear;
+echo "_________________";
+echo "nome: "$nome;
+echo "idade: "$idade;
+if [ $idade -lt 18 ]; then
+echo "menor de idade";
+else
+echo "maior de idade";
+fi
+echo "___________________________________";
+exit; 
